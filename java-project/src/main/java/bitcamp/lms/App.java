97140704a25b4c348c50bfa1ed7ad1703a4fb414
jeajoong <jeajoong605@gmail.com>
2019-01-07 10:11:57 +0900

    revise

--- java-project/src/main/java/bitcamp/lms/App.java
+++ java-project/src/main/java/bitcamp/lms/App.java
@@ -1,17 +1,39 @@
 package bitcamp.lms;
 
+import java.util.Scanner;
+
 public class App {
     
     public static void main(String[] args) {
-    System.out.println("번호:"+1 );
-    System.out.println("수업명:"+ "자바 프로젝트 실습");
-    System.out.println("설명:"+ "자바 프로젝트를 통한 자바 언어 활용법 익히기");
-    System.out.printf("시작일: 2019-01-02\n");
-    System.out.printf("종료일: 2019-05-28\n");
-    System.out.print("총수업시간:");
-    System.out.print(1000);
-    System.out.print(" 시간");
-    System.out.println();
-    System.out.printf("일수업시간: %d 시간\n", 8);
+      Scanner sc = new Scanner(System.in);
+      System.out.print("번호? ");
+      int no = sc.nextInt();
+      
+      sc.nextLine();
+      System.out.print("수업명? ");
+      String title = sc.nextLine();
+      
+      System.out.print("수업내용? ");
+      String speech = sc.nextLine();
+      
+      System.out.print("시작일? ");
+      String sdate = sc.nextLine();
+      
+      System.out.print("종료일? ");
+      String edate = sc.nextLine();
+      
+      System.out.print("총수업시간? ");
+      int ttime = sc.nextInt();
+      
+      System.out.print("일수업시간? ");
+      int dtime = sc.nextInt();
+      
+      System.out.printf("번호: %d\n", no);
+      System.out.printf("수업명: %s\n", title);
+      System.out.printf("수업내용 : %s\n", speech);
+      System.out.printf("시작일: %s\n", sdate);
+      System.out.printf("종료일: %s\n", edate);
+      System.out.printf("총수업시간: %s\n", ttime);
+      System.out.printf("일수업시간: %s\n", dtime);
     }
     }
