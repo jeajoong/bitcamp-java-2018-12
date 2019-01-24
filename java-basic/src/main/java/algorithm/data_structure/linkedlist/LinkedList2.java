ee1014bddf24b02256d46b5b008b46e1ae624461
jeajoong <jeajoong605@gmail.com>
2019-01-24 11:47:06 +0900

    add

--- java-basic/src/main/java/algorithm/data_structure/linkedlist/LinkedList2.java
+++ java-basic/src/main/java/algorithm/data_structure/linkedlist/LinkedList2.java
@@ -11,10 +11,25 @@ public class LinkedList2 {
     size = 0;
   }
   public void add(Object value) {
-
+    tail.value = value;
+    
+    Node node = new Node();
+    node.prev = tail;
+    tail.next = node;
+    tail = node;
+    return;
   }
-  public Object get(int index) {
+  public Object get(int index) { // (index)번호를 주고 해당 노드의 값을 반환해줌
+    if (index < 0 || index >= size)
+      return null;
+    
+    Node cursor = head;
 
+     for (int i = 0; i < index; i++) {
+       cursor = cursor.next;
+      
+    }
+     return cursor.value;
   }
   
   public Object[] toArray() {
