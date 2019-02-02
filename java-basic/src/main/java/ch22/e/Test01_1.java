255cecbe50de48b3f887866054d50c06088fb07c
JeaJoong <46276528+jeajoong@users.noreply.github.com>
2019-02-02 19:55:39 +0900

    이름은 저장되는데 점수가 문자로 저장됨..

--- java-basic/src/main/java/ch22/e/Test01_1.java
+++ java-basic/src/main/java/ch22/e/Test01_1.java
@@ -1,16 +1,14 @@
 // 인스턴스 출력하기
 package ch22.e;
 
+import java.io.BufferedOutputStream;
 import java.io.DataOutputStream;
-import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
-import java.io.FileWriter;
-import java.io.IOException;
 
-public class Test01_1 {
+public class Test01_1{
   
-  FileOutputStream out = null;
-
+  static FileOutputStream out = null;
+  
   public static void main(String[] args) {
     
     // 다음 세 학생의 성적 정보를 score.data 파일에 바이너리 형식으로 저장하라!
@@ -20,26 +18,33 @@ public class Test01_1 {
     Score s1 = new Score("홍길동", 100, 100, 100);
     Score s2 = new Score("임꺽정", 90, 90, 90);
     Score s3 = new Score("유관순", 80, 80, 80);
-
     
-    try (FileOutputStream out1 = new FileOutputStream("score.data");
-        DataOutputStream out2 = new DataOutputStream(out1)
-            ) {
+    try (FileOutputStream out = new FileOutputStream("Score.data");
+        BufferedOutputStream out1 = new BufferedOutputStream(out);
+        DataOutputStream out2 = new DataOutputStream(out1)  ) {
       
-        out1.writeUTF(s1.setName("홍길동"));
-  
-        
-        
+      // try (DataOutputStream out = new DataOutputStream(new BufferedOutputStream(
+     //                                                                 new FileOutputStream("Score.data"))) ) {
+      
+      
+      out2.writeUTF(s1.getName());
+      out2.writeInt(s1.getKor());
+      out2.writeInt(s1.getEng());
+      out2.writeInt(s1.getMath());
+      
+      out2.writeUTF(s2.getName());
+      out2.writeInt(s2.getKor());
+      out2.writeInt(s2.getEng());
+      out2.writeInt(s2.getMath());
+      
+      out2.writeUTF(s2.getName());
+      out2.writeInt(s2.getKor());
+      out2.writeInt(s2.getEng());
+      out2.writeInt(s2.getMath());
+      
     } catch (Exception e) {
       e.printStackTrace();
     
       }
   }
-  
-  public void writeUTF(String value) throws IOException {
-    byte[] bytes = value.getBytes("UTF-8");
-    out.write(bytes.length >> 8);
-    out.write(bytes.length);
-    out.write(bytes);
-}
 }
