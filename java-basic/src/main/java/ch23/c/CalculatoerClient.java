8015767c52399acca0bdbb17b04c76fb08579b06
JeaJoong <46276528+jeajoong@users.noreply.github.com>
2019-02-09 00:36:41 +0900

    done?

--- java-basic/src/main/java/ch23/c/CalculatoerClient.java
+++ java-basic/src/main/java/ch23/c/CalculatoerClient.java
@@ -1,10 +1,6 @@
 //계산기 클라이언트 만들기 : 최소 + , - , * , / , % 연산자는 지원한다.
 package ch23.c;
 
-import java.io.BufferedInputStream;
-import java.io.BufferedOutputStream;
-import java.io.DataInputStream;
-import java.io.DataOutputStream;
 import java.io.PrintWriter;
 import java.net.Socket;
 import java.util.Scanner;
@@ -12,52 +8,44 @@ import java.util.Scanner;
 /*
 실행 예:
 - 클라이언트가 계산기 서버에 접속한 후
-
 계산기 서버에 오신 걸 환영합니다! <==== 서버의 응답 
  계산식을 입력하세요! <==== 서버의 응답 
  예) 23 + 7 <==== 서버의 응답 
 >  23 + 7 <==== 사용자의 입력. '>' 문자는 클라이언트에서 출력한다.
 결과는 30 입니다. <==== 서버의 응답 
-
 > 23 ^ 7 <==== 사용자의 입력. '>' 문자는 클라이언트에서 출력한다.
 ^ 연산자를 지원하지 않습니다. <==== 서버의 응답 
-
 > ok + yes <==== 사용자의 입력. '>' 문자는 클라이언트에서 출력한다.
 식의 형식이 잘못되었습니다. <==== 서버의 응답 
-
 > quit <==== 사용자의 입력. '>' 문자는 클라이언트에서 출력한다.
 안녕히 가세요! <==== 서버의 응답
  */
 
 public class CalculatoerClient {
-  
-  static Scanner keyboard = new Scanner(System.in);
-
   public static void main(String[] args) {
     
-    try (Socket socket = new Socket("localhost", 8888);
-        DataOutputStream out = new DataOutputStream(
-            new BufferedOutputStream(socket.getOutputStream()));
+    try (Scanner keyboard = new Scanner(System.in);
+        Socket socket = new Socket("localhost", 8888);
         Scanner in = new Scanner(socket.getInputStream());
+        PrintWriter out = new PrintWriter(socket.getOutputStream())
        ) {
       
       while (true) {
-        String prompt = command();
-        out.writeInt(in.nextInt());
-        out.writeUTF(in.next());
-        out.writeInt(in.nextInt());
-        if (prompt.equals("quit")) {
-          out.writeUTF("quit");
+        System.out.print("> ");
+        String command = "";
+        command = keyboard.nextLine();
+        if (command.equalsIgnoreCase("quit")) {
+          out.println("quit");
           break;
         }
+        out.println(command);
+        out.flush();
+        out.println("클라이언트에서 데이터를 전송하였습니다.");
+        out.flush();
+        String result = in.nextLine();
+        System.out.println(result);
+      }
+    } catch (Exception e) {
     }
-  }catch (Exception e) {
-}
-}
-   private static String command() {
-    System.out.print("> ");
-    return keyboard.nextLine().toLowerCase();
   }
-  
-  
 }
--- java-basic/src/main/java/ch23/c/CalculatoerServer.java
+++ java-basic/src/main/java/ch23/c/CalculatoerServer.java
@@ -6,14 +6,13 @@ import java.io.DataInputStream;
 import java.io.PrintWriter;
 import java.net.ServerSocket;
 import java.net.Socket;
-import java.util.Scanner;
 
 public class CalculatoerServer {
 
   public static void main(String[] args) {
     
-    try (Scanner keyboard = new Scanner(System.in);
-        ServerSocket serverSocket = new ServerSocket(8888)) {
+    try (ServerSocket serverSocket = new ServerSocket(8888)) {
+      
       try (
           Socket socket = serverSocket.accept();
           PrintWriter out = new PrintWriter(socket.getOutputStream());
@@ -21,50 +20,48 @@ public class CalculatoerServer {
           new BufferedInputStream(socket.getInputStream()))
           ) {
       
-        out.write("계산기 서버에 오신 걸 환영합니다!");
-        out.write("계산식을 입력하세요!");
-        out.write("예) 23 + 7"); 
-        
+        out.println("계산기 서버에 오신 걸 환영합니다!");
+        out.flush();
+        out.println("계산식을 입력하세요!");
+        out.flush();
+        out.println("예) 23 + 7");
+        out.flush();
         
         while (true) {
-        
         int a = in.readInt();
         String op = in.readUTF();
         int b = in.readInt();
         
         float result = 0;
-        
         switch (op) {
           case "+": result = plus(a, b); break;
           case "-": result = minus(a, b); break;
           case "*": result = multiple(a, b); break;
           case "/": result = divide(a, b); break;
           case "%": result = pct(a, b); break;
-          default:
-            return;
+          default: return ;
         }
-        out.write((int) result);
+        out.write("결과는 "+ (int) result +"입니다.");
+        out.flush();
         
-        if(in.readUTF() == "^") {
+        if(op == "^") {
           out.write("^ 연산자를 지원하지 않습니다.");
         }
-        
-        else {
-          out.write("식의 형식이 잘못되었습니다.");
-        }
-        
+        try {
          if (in.readUTF() == "quit")
           out.write("안녕히 가세요!");
         break;
-        
+      
+        } catch (NumberFormatException e) {
+        out.println("식의 형식이 잘못되었습니다.");
+          }
+      } 
       }
-      }
-  }catch (Exception e) {
+    }catch (Exception e) {
+    e.printStackTrace();
   }
-}
-  
-  
-  
+  }
+
   public static int plus(int a, int b) {
     return a+b;
   }
@@ -82,5 +79,3 @@ public class CalculatoerServer {
   }
   
 }
-
-
