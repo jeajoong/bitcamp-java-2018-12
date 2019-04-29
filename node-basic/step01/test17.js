3ca729d5e65a81c5be90fde74fd53a12731398c4
jeajoong <46276528+jeajoong@users.noreply.github.com>
2019-04-29 21:49:43 +0900

    Update test17.js

--- node-basic/step01/test17.js
+++ node-basic/step01/test17.js
@@ -9,6 +9,7 @@
 var a1 = [["홍길동", 99, 98, 97], ["임꺽정", 89, 88, 87], ["유관순", 79, 78, 77]];
 
 // 임꺽정의 수학점수?
+console.log(a1[1][0]);
 console.log(a1[1][3]);
 console.log(a1.length);
 console.log(a1[2].length);
