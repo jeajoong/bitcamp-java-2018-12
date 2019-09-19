6226f6c7b345be5719b75eca0ae31939b457203b
JeaJoong <46276528+jeajoong@users.noreply.github.com>
2019-09-19 14:17:49 +0900

    serialVersionUID설정해줘야하는이유

--- java-web/src/main/java/bitcamp/ex01/Servlet02.java
+++ java-web/src/main/java/bitcamp/ex01/Servlet02.java
@@ -15,6 +15,11 @@ public class Servlet02 extends GenericServlet {
   
   // GenericServlet 추상 클래스는 java.io.Serialize 인터페이스를 구현하였다.
   // => serialVersionUID 변수 값을 설정해야 한다.
+  //    - 객체를 저장하고 불러올때 이 serialVersionUID 값을 가지고 불러온다는 뜻이며
+  //    - serialVersionUID의 값을 직접 할당하지 않고 컴파일러에게 맡긴다면 
+  //       아래의 경우 제대로 객체를 불러올 수 없을 것이다.
+  //    ● 저장하는 쪽의 컴파일러와 불러오는 쪽의 컴파일러가 다를 경우
+  //    ● 저장하는 시기의 클래스 내용과 불러오는 시기의 클래스 내용이 다를 경우
   private static final long serialVersionUID = 1L;
 
   
